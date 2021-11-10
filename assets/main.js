df8a6c6399f57c0e4cd306447b9dc4fa67b4f9ca
BarKarMa <bezuhlov2andrii@gmail.com>
2021-11-10 17:56:29 +0200

    width 50%

--- assets/main.js
+++ assets/main.js
@@ -43,7 +43,8 @@ document.addEventListener('submit', (e) =>{
 
 socket.on('chat message', (data) => {
   const item1 = document.createElement('div')
-  item1.innerHTML = `<p style="margin-left:100px">${formattedTime}</p>`
+  item1.innerHTML = `<p style="width:50%">${formattedTime}</p>`
+  //item1.innerHTML = `<p style="margin-left:100px">${formattedTime}</p>`
   
   messages.appendChild(item1)
   const item = document.createElement('div')
