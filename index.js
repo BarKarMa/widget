11b0edb318b0381606a4c0fd40e03ab566b7b7a6
BarKarMa <bezuhlov2andrii@gmail.com>
2021-11-10 14:21:08 +0200

    try 36

--- index.js
+++ index.js
@@ -73,11 +73,15 @@ app.post("/test", (req, res) => {
   
   console.log('full output')
   console.log(req.body);
+
+  if(!contype)
+    return res.sendStatus(400)
   
-  if (!contype || contype.indexOf('application/x-www-form-urlencoded; charset=UTF-8') !== 0)
+  if (contype.indexOf('application/x-www-form-urlencoded; charset=UTF-8') !== 0)
     io.emit('chat message', req.body);
     console.log("2");
     return res.sendStatus(200);
+  
   
   //io.emit('chat message', req.body);
   // сдесь иф который будет отправлять только когда идет запрос через АПИ
