d016b624f8738faaa646b36c2c58af702f66c7e3
BarKarMa <bezuhlov2andrii@gmail.com>
2021-11-16 23:05:05 +0200

    ssend test

--- assets/main.js
+++ assets/main.js
@@ -34,10 +34,15 @@ document.addEventListener('submit', (e) =>{
         name,
         numbers
       }
-      $.post('/test', data, function() {
-          console.log('server recieved data')
-      });
+      
+      //$.post('/test', data, function() {
+      //    console.log('server recieved data')
+      //});
+      
 
+
+      $.post('https://balance.beesender.com/api/v1.0/sendmessage/5673e2ff-da23-4db4-8da1-963abfdf1395/39871eae-1695-46fe-b8e2-f91597c7a89a', {'sender': { 'id': '413242354', "name": '+380992472015', 'avatar': ''}, 'message': {'type': 'text', 'text': 'Вітаю'}}, function(){});
+        
       //$.post('https://balance.beesender.com/api/v1.0/sendmessage/5673e2ff-da23-4db4-8da1-963abfdf1395/39871eae-1695-46fe-b8e2-f91597c7a89a',
         //{'sender': { 'id': '413242354', "name": '+380992472015', 'avatar': ''}, 'message': {'type': 'text', 'text': 'Вітаю'}},
         //function (error, response, body) {
