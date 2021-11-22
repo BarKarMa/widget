ce6179eb534e0c740fdec4bd46db88417bbc83e4
A_Bezuhlov <bezuhlov2andrii@gmail.com>
2021-11-22 17:14:43 +0200

    try

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ app.post("/test", (req, res) => {
   //console.log(res.json({"message": "message recieved"}))
   //if(!req.body) return res.sendStatus(400);
   var contype = req.headers['content-type']
-  console.log('room'+socket.id)
+  
   console.log('full output')
   console.log(req.body)
   //console.log('---------- head ------------')
@@ -120,6 +120,7 @@ app.post("/test", (req, res) => {
   
   if (contype.indexOf('application/x-www-form-urlencoded; charset=UTF-8') !== 0)
     console.log('room')
+    console.log(io.id)
     
     
     io.to('room'+socket.id).emit('chat message', req.body)
