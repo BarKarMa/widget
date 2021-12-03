b4b941c373023d9ff7d81e32a4d9ce19bc52d9aa
BarKarMa <bezuhlov2andrii@gmail.com>
2021-12-03 20:18:06 +0200

    try

--- assets/main.js
+++ assets/main.js
@@ -12,7 +12,7 @@ const time = new Date();
 const formattedTime = time.toLocaleString("en-US", { hour: "numeric", minute: "numeric", second: "numeric" });
 
 //nameBlock.innerHTML = `${userName}`
-const item00 = document.createElement('div style="margin-top:20%"')
+const item00 = document.createElement('div class="hello"')
 item00.innerHTML = `<span class="hello-messages" style="text-align: center; ">Вітаємо вас у контактному центрі! Для зв'язку з оператором надішліть повідомлення.</span>`
 messages.appendChild(item00)
 
