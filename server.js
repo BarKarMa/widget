46b7cf80487479767677ac492bf21f769eb79e49
A_Bezuhlov <bezuhlov2andrii@gmail.com>
2021-12-06 12:25:49 +0200

    ?&

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const timeout = require('connect-timeout')
 const homeRoute = require('./src/routes.js')
 const chatRoute = require('./src/routes.js')
 
+app.use('/', homeRoute)
+app.use('/newChat', chatRoute)
+
 //
 
 const { request } = require('http');
@@ -84,36 +87,6 @@ app.use(express.static(__dirname + '/src'))
 app.use(express.static(path.join(__dirname)));
 app.use("/styles", express.static(__dirname + '/styles'));
 
-
-
-// перенос в SRC/routes
-app.get("/", (req, res) => {
-  console.log("1");
-  res.sendFile(path.join(__dirname, '/index.html'))
-});
-
-app.use('/', homeRoute)
-app.use('/newChat', chatRoute)
-
-// app.post("/test", (req, res) => {
-//   try {
-
-//   var contype = req.headers['content-type']
-  
-//   console.log('full output')
-//   console.log(req.body)
-
-//   if(!contype)
-//     return res.sendStatus(400)
-//   if (contype.indexOf('application/x-www-form-urlencoded; charset=UTF-8') !== 0)
-//     io.to('room'+req.body.receiver_id).emit('chat message', req.body)
-//     console.log("2");
-//     return res.sendStatus(200);
-//   }  catch(error) {
-    
-//   }
-
-// });
 
 
 
