19ada6416bcdb56879d3270c0547defe378af8c7
A_Bezuhlov <bezuhlov2andrii@gmail.com>
2021-12-06 15:45:28 +0200

    try

--- server.js
+++ server.js
@@ -9,9 +9,10 @@ const bodyParser = require('body-parser')
 const io = require('socket.io')(http)
 
 // раути АПИ
-const homeRoute = require('./src/routes.js')
-app.use('/', homeRoute)
-app.use('/newChat', homeRoute)
+// const homeRoute = require('./src/routes.js')
+// const newRoute = require('./src/routes.js')
+// app.use('/', homeRoute)
+// app.use('/newChat', newRoute)
 
 // підключення сокетів
 //const sock = require('/src/socket-io.js')(http);
@@ -73,6 +74,35 @@ socket.on('typing', () => {
 })
   
 })
+
+
+app.get("/", (req, res) => {
+  console.log("1");
+  res.sendFile(path.join(__dirname, '/index.html'))
+});
+
+
+app.post("/newChat", (req, res) => {
+  try {
+    //const sock = require('/socket-io.js')
+
+    var contype = req.headers['content-type']
+    
+    console.log('full output')
+    console.log(req.body)
+
+    if(!contype)
+      return res.sendStatus(400)
+    if (contype.indexOf('application/x-www-form-urlencoded; charset=UTF-8') !== 0)
+      io.to('room'+req.body.receiver_id).emit('chat message', req.body)
+      console.log("2");
+      return res.sendStatus(200);
+    }  catch(error) {
+      
+    }
+
+});
+
 
 
 
