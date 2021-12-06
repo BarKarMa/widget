2cec359c3318b639466efa872f2fa2cfea074d4b
A_Bezuhlov <bezuhlov2andrii@gmail.com>
2021-12-06 18:04:31 +0200

    try

--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,7 @@
-//const socket = io()
+const socket = io()
 
-const socket = require('./server.js').io
+//const socket = require('./server.js').io
+
 const messages = document.querySelector('.messages')
 // ('#messages')
 // getByDataHook(dataHook) { return document.querySelector('[data-hook="ChatWindow.messagesWrapper"]'); }
