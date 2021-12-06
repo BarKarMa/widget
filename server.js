444712088efb7b6fe0571c06bfd0d67b947a23da
A_Bezuhlov <bezuhlov2andrii@gmail.com>
2021-12-06 15:40:24 +0200

    try

--- server.js
+++ server.js
@@ -31,47 +31,46 @@ app.use("/styles", express.static(__dirname + '/styles'));
 
 
   
-  users = []
-  let clients = []
-  let numUsers = 0;
+users = []
+let clients = []
+let numUsers = 0;
 
 
-  io.on('connection', (socket) => {
-  console.log(`Client with id ${socket.id} connected`)
-  console.log(`/////////////////////////////////////////`)
-  clients.push(socket.id)
-  ++numUsers;
-  console.log(`Num of users: ${numUsers}`)
+io.on('connection', (socket) => {
+console.log(`Client with id ${socket.id} connected`)
+console.log(`/////////////////////////////////////////`)
+clients.push(socket.id)
+++numUsers;
+console.log(`Num of users: ${numUsers}`)
   
-  socket.join("room"+socket.id)
+socket.join("room"+socket.id)
   
   
-  socket.on('chat message', (data) =>{
+socket.on('chat message', (data) =>{
     
     
-    console.log(data)
-    io.to("room"+socket.id).emit('chat message', {
-      message: data.message,
-      name: data.name,
-    })
+  console.log(data)
+  io.to("room"+socket.id).emit('chat message', {
+    message: data.message,
+    name: data.name,
+  })
       
+})
+
+socket.on('disconnect', (data) => {
+  --numUsers;
+  console.log(`User disconnected. On server are: ${numUsers}`)
+  io.emit('user-disconnected', users[socket.id])
+  delete users[socket.id]
+  console.log(`'user-disconnected',  ${socket.id}`)
+  socket.leave("room"+socket.id);
   })
 
-  socket.on('disconnect', (data) => {
-    --numUsers;
-    console.log(`User disconnected. On server are: ${numUsers}`)
-    io.emit('user-disconnected', users[socket.id])
-    delete users[socket.id]
-    console.log(`'user-disconnected',  ${socket.id}`)
-    socket.leave("room"+socket.id);
-    })
-
-  socket.on('typing', () => {
-    socket.emit('typing', {
-      username: socket.username
-    })
+socket.on('typing', () => {
+  socket.emit('typing', {
+    username: socket.username
   })
-  
+})
   
 })
 
