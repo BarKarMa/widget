5e0412600c238f9ef3f6fbb5578664209e59cbb7
A_Bezuhlov <bezuhlov2andrii@gmail.com>
2021-12-06 13:57:53 +0200

    try

--- src/client/client.js
+++ src/client/client.js
@@ -11,15 +11,13 @@ const nameBlock = document.querySelector('.name')
 
 //const userName = prompt('Ваше імя:')
 
-//
-
 
 
 const time = new Date();
 
 
 const formattedTime = time.toLocaleString("en-US", { hour: "numeric", minute: "numeric", second: "numeric" });
- const item_hello = document.createElement('div')
+const item_hello = document.createElement('div')
 item_hello.innerHTML = `<div><p class="hello-messages" style="text-align: center;">Вітаємо вас у контактному центрі! Для зв'язку з оператором надішліть повідомлення.</p></div>`
 messages.appendChild(item_hello)
 
--- src/routes.js
+++ src/routes.js
@@ -11,20 +11,20 @@ app.get("/", (req, res) => {
 app.post("/newChat", (req, res) => {
   try {
 
-  var contype = req.headers['content-type']
-  
-  console.log('full output')
-  console.log(req.body)
-
-  if(!contype)
-    return res.sendStatus(400)
-  if (contype.indexOf('application/x-www-form-urlencoded; charset=UTF-8') !== 0)
-    io.to('room'+req.body.receiver_id).emit('chat message', req.body)
-    console.log("2");
-    return res.sendStatus(200);
-  }  catch(error) {
+    var contype = req.headers['content-type']
     
-  }
+    console.log('full output')
+    console.log(req.body)
+
+    if(!contype)
+      return res.sendStatus(400)
+    if (contype.indexOf('application/x-www-form-urlencoded; charset=UTF-8') !== 0)
+      io.to('room'+req.body.receiver_id).emit('chat message', req.body)
+      console.log("2");
+      return res.sendStatus(200);
+    }  catch(error) {
+      
+    }
 
 });
 
