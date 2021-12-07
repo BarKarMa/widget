15d4d01b003059a54f689324c3e6c493797b82d1
BarKarMa <bezuhlov2andrii@gmail.com>
2021-12-07 16:51:06 +0200

    try

--- server.js
+++ server.js
@@ -37,34 +37,35 @@ app.use("/styles", express.static(__dirname + '/styles'));
 
 // сама ініціалізація
 
+require('/app/src/client/client.js')(app)
 
-app.get("/", (req, res) => {
-  console.log("1");
-  res.sendFile(path.join(__dirname, '/index.html'))
-});
+// app.get("/", (req, res) => {
+//   console.log("1");
+//   res.sendFile(path.join(__dirname, '/index.html'))
+// });
 
 
-app.post("/newChat", (req, res) => {
-  try {
-    //const sock = require('/socket-io.js')
+// app.post("/newChat", (req, res) => {
+//   try {
+//     //const sock = require('/socket-io.js')
 
-    var contype = req.headers['content-type']
+//     var contype = req.headers['content-type']
     
-    console.log('full output')
-    console.log(req.body)
+//     console.log('full output')
+//     console.log(req.body)
 
-    if(!contype)
-      return res.sendStatus(400)
-    if (contype.indexOf('application/x-www-form-urlencoded; charset=UTF-8') !== 0)
-      //socket.to('room'+req.body.receiver_id).emit('chat message', req.body)
-      sock.to('room'+req.body.receiver_id).emit('chat message', req.body)
-      console.log("2");
-      return res.sendStatus(200);
-    }  catch(error) {
+//     if(!contype)
+//       return res.sendStatus(400)
+//     if (contype.indexOf('application/x-www-form-urlencoded; charset=UTF-8') !== 0)
+//       //socket.to('room'+req.body.receiver_id).emit('chat message', req.body)
+//       sock.to('room'+req.body.receiver_id).emit('chat message', req.body)
+//       console.log("2");
+//       return res.sendStatus(200);
+//     }  catch(error) {
       
-    }
+//     }
 
-});
+// });
 
 
 const port = 3000;
--- src/routes.js
+++ src/routes.js
@@ -1,40 +1,41 @@
-const express = require('express');
-const router = express.Router();
-const app = express();
+//const express = require('express');
+//const router = express.Router();
+//const app = express();
 
 //const http = require('/app/server.js')
 //const io = require('socket.io')(http)
 // const sock = require('/app/src/socket_io.js')(io)
 
+exports = module.exports = function (app) {
 
-router.get("/", (req, res) => {
-  console.log("1");
-  res.sendFile(path.join(__dirname, '/index.html'))
-});
-
-
-app.post("/newChat", (req, res) => {
-    try {
-
-        
-      var contype = req.headers['content-type']
-      
-      console.log('full output')
-      console.log(req.body)
-
-      if(!contype)
-        return res.sendStatus(400)
-      if (contype.indexOf('application/x-www-form-urlencoded; charset=UTF-8') !== 0)
-        sock.to('room'+req.body.receiver_id).emit('chat message', req.body)
-        console.log("2");
-        return res.sendStatus(200);
-      }  catch(error) {
-        
-      }
-
+  app.get("/", (req, res) => {
+    console.log("1");
+    res.sendFile(path.join(__dirname, '/index.html'))
   });
 
 
+  app.post("/newChat", (req, res) => {
+      try {
+
+          
+        var contype = req.headers['content-type']
+        
+        console.log('full output')
+        console.log(req.body)
+
+        if(!contype)
+          return res.sendStatus(400)
+        if (contype.indexOf('application/x-www-form-urlencoded; charset=UTF-8') !== 0)
+          sock.to('room'+req.body.receiver_id).emit('chat message', req.body)
+          console.log("2");
+          return res.sendStatus(200);
+        }  catch(error) {
+          
+        }
+
+    });
+
+}
 
 module.exports = router;
 
