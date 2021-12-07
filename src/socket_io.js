ceabceb3ae738e21969bd422d174ecb90586482f
BarKarMa <bezuhlov2andrii@gmail.com>
2021-12-07 19:14:46 +0200

    try

--- src/socket_io.js
+++ src/socket_io.js
@@ -7,40 +7,40 @@ exports = module.exports = function(io) {
 
 
   io.on('connection', (socket) => {
-  console.log(`Client with id ${socket.id} connected`)
-  console.log(`/////////////////////////////////////////`)
-  clients.push(socket.id)
-  ++numUsers;
-  console.log(`Num of users: ${numUsers}`)
-  
-  socket.join("room"+socket.id)
-  
-  
-  socket.on('chat message', (data) =>{
+    console.log(`Client with id ${socket.id} connected`)
+    console.log(`/////////////////////////////////////////`)
+    clients.push(socket.id)
+    ++numUsers;
+    console.log(`Num of users: ${numUsers}`)
     
-    
-    console.log(data)
-    io.to("room"+socket.id).emit('chat message', {
-      message: data.message,
-      name: data.name,
-    })
+    socket.join("room"+socket.id)
+  
+  
+    socket.on('chat message', (data) =>{
       
-  })
-
-  socket.on('disconnect', (data) => {
-    --numUsers;
-    console.log(`User disconnected. On server are: ${numUsers}`)
-    io.emit('user-disconnected', users[socket.id])
-    delete users[socket.id]
-    console.log(`'user-disconnected',  ${socket.id}`)
-    socket.leave("room"+socket.id);
+      
+      console.log(data)
+      io.to("room"+socket.id).emit('chat message', {
+        message: data.message,
+        name: data.name,
+      })
+        
     })
 
-  socket.on('typing', () => {
-    socket.emit('typing', {
-      username: socket.username
+    socket.on('disconnect', (data) => {
+      --numUsers;
+      console.log(`User disconnected. On server are: ${numUsers}`)
+      io.emit('user-disconnected', users[socket.id])
+      delete users[socket.id]
+      console.log(`'user-disconnected',  ${socket.id}`)
+      socket.leave("room"+socket.id);
+      })
+
+    socket.on('typing', () => {
+      socket.emit('typing', {
+        username: socket.username
+      })
     })
-  })
   
   
 })
