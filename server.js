0f04e20586447b8fbdbd01ebff74fa30996bd909
BarKarMa <bezuhlov2andrii@gmail.com>
2021-12-07 21:21:55 +0200

    try

--- server.js
+++ server.js
@@ -23,6 +23,46 @@ app.use("/src/client", express.static(__dirname + '/src/client'))
 app.use(express.static(path.join(__dirname)));
 app.use("/styles", express.static(__dirname + '/styles'));
 
+
+users = []
+  let clients = []
+  let numUsers = 0;
+
+
+  io.on('connection', (socket) => {
+    console.log(`Client with id ${socket.id} connected`)
+    console.log(`/////////////////////////////////////////`)
+    clients.push(socket.id)
+    ++numUsers;
+    console.log(`Num of users: ${numUsers}`)
+    
+    socket.join("room"+socket.id)
+  
+    socket.on('chat message', (data) =>{
+      
+      console.log(data)
+      io.to("room"+socket.id).emit('chat message', {
+        message: data.message,
+        name: data.name,
+      })
+        
+    })
+
+    socket.on('disconnect', (data) => {
+      --numUsers;
+      console.log(`User disconnected. On server are: ${numUsers}`)
+      io.emit('user-disconnected', users[socket.id])
+      delete users[socket.id]
+      console.log(`'user-disconnected',  ${socket.id}`)
+      socket.leave("room"+socket.id);
+      })
+
+    socket.on('typing', () => {
+      socket.emit('typing', {
+        username: socket.username
+      })
+    })
+})
 // сама ініціалізація
 //require('/app/src/routes.js')(app)
 
