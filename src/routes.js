c8e0adb0af55d6fa40c211e0c819f85c95b9ec40
BarKarMa <bezuhlov2andrii@gmail.com>
2021-12-07 16:40:11 +0200

    try

--- src/routes.js
+++ src/routes.js
@@ -13,26 +13,29 @@ router.get("/", (req, res) => {
 });
 
 
-router.post("/newChat", (req, res) => {
-  try {
+exports = module.exports = function post(app){
+  app.post("/newChat", (req, res) => {
+    try {
 
+        
+      var contype = req.headers['content-type']
       
-    var contype = req.headers['content-type']
-    
-    console.log('full output')
-    console.log(req.body)
+      console.log('full output')
+      console.log(req.body)
 
-    if(!contype)
-      return res.sendStatus(400)
-    if (contype.indexOf('application/x-www-form-urlencoded; charset=UTF-8') !== 0)
-      sock.to('room'+req.body.receiver_id).emit('chat message', req.body)
-      console.log("2");
-      return res.sendStatus(200);
-    }  catch(error) {
-      
-    }
+      if(!contype)
+        return res.sendStatus(400)
+      if (contype.indexOf('application/x-www-form-urlencoded; charset=UTF-8') !== 0)
+        sock.to('room'+req.body.receiver_id).emit('chat message', req.body)
+        console.log("2");
+        return res.sendStatus(200);
+      }  catch(error) {
+        
+      }
 
-});
+  });
+
+}
 
 module.exports = router;
 
