6dcab79fe37755601f51e5d7e1efaa505b8236f3
BarKarMa <bezuhlov2andrii@gmail.com>
2021-12-07 13:36:01 +0200

    try

--- server.js
+++ server.js
@@ -34,51 +34,6 @@ app.use("/styles", express.static(__dirname + '/styles'));
 // сама ініціалізація
 
 
-//const io = require('socket.io')(http)
-// users = []
-// let clients = []
-// let numUsers = 0;
-
-
-// io.on('connection', (socket) => {
-// console.log(`Client with id ${socket.id} connected`)
-// console.log(`/////////////////////////////////////////`)
-// clients.push(socket.id)
-// ++numUsers;
-// console.log(`Num of users: ${numUsers}`)
-  
-// socket.join("room"+socket.id)
-  
-  
-// socket.on('chat message', (data) =>{
-    
-    
-//   console.log(data)
-//   io.to("room"+socket.id).emit('chat message', {
-//     message: data.message,
-//     name: data.name,
-//   })
-      
-// })
-
-// socket.on('disconnect', (data) => {
-//   --numUsers;
-//   console.log(`User disconnected. On server are: ${numUsers}`)
-//   io.emit('user-disconnected', users[socket.id])
-//   delete users[socket.id]
-//   console.log(`'user-disconnected',  ${socket.id}`)
-//   socket.leave("room"+socket.id);
-//   })
-
-// socket.on('typing', () => {
-//   socket.emit('typing', {
-//     username: socket.username
-//   })
-// })
-  
-// })
-
-
 app.get("/", (req, res) => {
   console.log("1");
   res.sendFile(path.join(__dirname, '/index.html'))
