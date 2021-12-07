06ed65ec447463adb076a437931d3aafbc66dc8a
BarKarMa <bezuhlov2andrii@gmail.com>
2021-12-07 19:42:57 +0200

    tr6

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.post("/newChat", (req, res) => {
       console.log(req.body)
 
       if(!contype)
-        return res.sendStatus(400)
+        return res.sendStatus(200)
       if (contype.indexOf('application/x-www-form-urlencoded; charset=UTF-8') !== 0)
         sock.to('room'+req.body.receiver_id).emit('chat message', req.body)
         console.log("2");
