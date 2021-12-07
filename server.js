29d22d12d3bfb5f191e90fe1518cc67580ae45cf
BarKarMa <bezuhlov2andrii@gmail.com>
2021-12-07 19:53:47 +0200

    huj

--- server.js
+++ server.js
@@ -26,6 +26,9 @@ app.use("/styles", express.static(__dirname + '/styles'));
 // сама ініціалізація
 //require('/app/src/routes.js')(app)
 
+const port = 3000;
+
+
 app.get("/", (req, res) => {
   console.log("1");
   res.sendFile(path.join(__dirname, '/index.html'))
@@ -36,23 +39,22 @@ app.post("/newChat", (req, res) => {
   try {
     var contype = req.headers['content-type']
       
-      console.log('full output')
-      console.log(req.body)
+    console.log('full output')
+    console.log(req.body)
 
-      if(!contype)
-        return res.sendStatus(400)
-      if (contype.indexOf('application/x-www-form-urlencoded; charset=UTF-8') !== 0)
-        sock.to('room'+req.body.receiver_id).emit('chat message', req.body)
-        console.log("2");
-        return res.sendStatus(200);
-      }  catch(error) {
+    if(!contype)
+      return res.sendStatus(400)
+    if (contype.indexOf('application/x-www-form-urlencoded; charset=UTF-8') !== 0)
+      sock.to('room'+req.body.receiver_id).emit('chat message', req.body)
+      console.log("2");
+      return res.sendStatus(200);
+    }  
+      
+  catch(error) {
         
       }
 
 });
-
-
-const port = 3000;
 
 http.listen(process.env.PORT || 3000, () => {
 
