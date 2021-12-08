0c05f31a698361f6ac5b2e4ed91151fb356c7767
A_Bezuhlov <bezuhlov2andrii@gmail.com>
2021-12-08 14:13:34 +0200

    try

--- src/client/client.js
+++ src/client/client.js
@@ -16,11 +16,11 @@ const nameBlock = document.querySelector('.name')
 const time = new Date();
 const formattedTime = time.toLocaleString("en-US", { hour: "numeric", minute: "numeric", second: "numeric" });
 const item_hello = document.createElement('div')
-item_hello.innerHTML = `<div><p class="hello-messages" style="text-align: center;">Вітаємо вас у контактному центрі! Для зв'язку з оператором надішліть повідомлення.</p></div>`
-messages.appendChild(item_hello)
 
 
-//require('/app/src/client/messages.js')(messages, item_hello)
+
+require('/app/src/client/messages.js')(messages, item_hello)
+
   // const formattedTime = time.toLocaleString("en-US", { hour: "numeric", minute: "numeric", second: "numeric" });
   // const item_hello = document.createElement('div')
   // item_hello.innerHTML = `<div><p class="hello-messages" style="text-align: center;">Вітаємо вас у контактному центрі! Для зв'язку з оператором надішліть повідомлення.</p></div>`
@@ -106,3 +106,9 @@ socket.on('chat message', (data) => {
 }
   
 )
+
+module.exports = {
+  messages: messages,
+  item_hello: item_hello
+  
+}
