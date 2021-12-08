ff6c5d7e0fad8b448959479cdfafae6bfbd5e58b
A_Bezuhlov <bezuhlov2andrii@gmail.com>
2021-12-08 13:51:04 +0200

    try

--- src/client/client.js
+++ src/client/client.js
@@ -18,12 +18,12 @@ const nameBlock = document.querySelector('.name')
 const time = new Date();
 
 
-const formattedTime = time.toLocaleString("en-US", { hour: "numeric", minute: "numeric", second: "numeric" });
-const item_hello = document.createElement('div')
-item_hello.innerHTML = `<div><p class="hello-messages" style="text-align: center;">Вітаємо вас у контактному центрі! Для зв'язку з оператором надішліть повідомлення.</p></div>`
-messages.appendChild(item_hello)
+// const formattedTime = time.toLocaleString("en-US", { hour: "numeric", minute: "numeric", second: "numeric" });
+// const item_hello = document.createElement('div')
+// item_hello.innerHTML = `<div><p class="hello-messages" style="text-align: center;">Вітаємо вас у контактному центрі! Для зв'язку з оператором надішліть повідомлення.</p></div>`
+// messages.appendChild(item_hello)
 
-
+require('/app/src/client/messages.js')(messages)
 
 //////
 function updateScroll(){
