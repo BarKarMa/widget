9d448e3036b085c462a44b3293f46899ec2ecbfe
A_Bezuhlov <bezuhlov2andrii@gmail.com>
2021-12-09 18:04:42 +0200

    try

--- src/client/client.js
+++ src/client/client.js
@@ -82,12 +82,12 @@ socket.on('chat message', (data) => {
 
   // вынести звідси код в окремий файл - ключ
   if (data.channel_id === "DCA20883-F093-4DA4-8FDC-9EAE03A51E18"){
-    item.innerHTML = `<p class="operator-name" style="text-align: right;">Оператор:</p> <span id="server-messages">${data.content.text}</span>`
+    item.innerHTML = `<p class="operator-name" style="text-align: right;">Оператор:(Online)</p> <span id="server-messages">${data.content.text}</span>`
     messages.appendChild(item)
     window.scrollTo(0,document.body.scrollHeight)
   }
   else {
-    item.innerHTML = `<p>Твоє ім'я:</p> <span id="my-messages"; ">${data.message}</span>`
+    item.innerHTML = `<p>ВИ:</p> <span id="my-messages"; ">${data.message}</span>`
 
     
     messages.appendChild(item)
