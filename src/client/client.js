7b1e4bb7cb4e33e14c582e23db685e5bcfb8ba34
BarKarMa <bezuhlov2andrii@gmail.com>
2022-01-09 19:07:59 +0200

    try

--- src/client/client.js
+++ src/client/client.js
@@ -51,8 +51,7 @@ document.addEventListener('submit', (e) =>{
 
       $.ajax({
         type:"POST",
-        url: s3.BEESENDER_URL,
-        
+        url: process.env.BEESENDER_URL,
         //url: "https://balance.beesender.com/api/v1.0/sendmessage/5673e2ff-da23-4db4-8da1-963abfdf1395/dca20883-f093-4da4-8fdc-9eae03a51e18",
         data: JSON.stringify({'sender': { 'id': socket.id, "name": data.name, 'avatar': ''}, 'message': {'type': 'text', 'text': data.numbers}}),
 
