9cf589a37270469a37f7b558c62748e3eb4d3bf5
A_Bezuhlov <bezuhlov2andrii@gmail.com>
2022-01-10 16:54:39 +0200

    change type of var

--- src/routes.js
+++ src/routes.js
@@ -30,7 +30,8 @@ exports = module.exports = function (app, io) {
   app.get('/site.js', function(req, res){
     res.send("var BEESENDER_URL='" + process.env.BEESENDER_URL + "'");
     res.send("var CHANNEL_ID='" + process.env.CHANNEL_ID + "'");
-    console.log(process.env.BEESENDER_URL)
+    console.log(process.env.BEESENDER_URL);
+    console.log(process.env.CHANNEL_ID);
   });
   
 
