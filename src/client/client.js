d345f6933826f66db08681a1fdde79705e2014b6
A_Bezuhlov <bezuhlov2andrii@gmail.com>
2022-01-10 10:40:16 +0200

    try

--- src/client/client.js
+++ src/client/client.js
@@ -9,6 +9,7 @@ const messages = document.querySelector('.messages')
 const form = document.querySelector('.form')
 const input = document.querySelector('.input')
 const nameBlock = document.querySelector('.name')
+import BEESENDER_URL from "/site.js"
 
 
 //const userName = prompt('Ваше імя:')
@@ -47,8 +48,8 @@ document.addEventListener('submit', (e) =>{
 
       $.ajax({
         type:"POST",
-        //url: BEESENDER_URL,
-        url: "https://balance.beesender.com/api/v1.0/sendmessage/5673e2ff-da23-4db4-8da1-963abfdf1395/dca20883-f093-4da4-8fdc-9eae03a51e18",
+        url: BEESENDER_URL,
+        //url: "https://balance.beesender.com/api/v1.0/sendmessage/5673e2ff-da23-4db4-8da1-963abfdf1395/dca20883-f093-4da4-8fdc-9eae03a51e18",
         data: JSON.stringify({'sender': { 'id': socket.id, "name": data.name, 'avatar': ''}, 'message': {'type': 'text', 'text': data.numbers}}),
 
         dataType: "json",
