75a26e69d4b590b6b628e57f19a8234c09880627
A_Bezuhlov <bezuhlov2andrii@gmail.com>
2022-02-03 12:30:51 +0200

    try

--- uploads/uploads.js
+++ uploads/uploads.js
@@ -1,6 +1,6 @@
 // import axios from 'axios'
 
-const API_URL = "http://localhost:5000"
+const API_URL = "https://pofsdvmlks.herokuapp.com"
 
 cloudinaryUpload = (fileToUpload) => {
     return axios.post(API_URL + '/cloudinary-upload', fileToUpload)
