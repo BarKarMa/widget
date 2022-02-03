8089f1a45e57deae65dd9dcd8afb9e0b3995cc5c
A_Bezuhlov <bezuhlov2andrii@gmail.com>
2022-02-03 13:26:07 +0200

    upd

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-exports = module.exports = function (app, io, nodemailer, fileUploader) {
+exports = module.exports = function (app, io, nodemailer, fileUploader, upload) {
 
   app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'))
